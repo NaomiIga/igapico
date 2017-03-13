3b1ae5b132d99d79b8d002b639d43f6bc295c70a
tekupeko <support-tekupeko@mxdlab.net>
2017-03-13 18:01:44 +0900

    add database_model

--- tekupico/cms/models.py
+++ tekupico/cms/models.py
@@ -4,12 +4,29 @@ from django.db import models
 
 # Create your models here.
 #default ha nyuuryoku ga kara no toki ni hyouji sareru moji
-class Data(models.Model):
-	userdata = models.CharField('username', max_length = 255, default = 'NAME')
-	datavalue = models.CharField('shop', max_length = 255, default = 'SHOP')
-
 class User(models.Model):
 	username = models.CharField('username', max_length = 255, default = 'NAME')
 	starttime = models.IntegerField('starttime', default = 0)
 	finishtime = models.IntegerField('finishtime', default = 0)
-	hint_action1_1 = models.BooleanField('hint1_1', default = False)
+	hint_action1_1 = models.IntegerField('hint1_1', default = False)
+
+class Treasure_Beacon(models.Model):
+	treasure = models.IntegerField('treasure', default = 0)
+	beacon = models.IntegerField('beacon', default = 0)
+
+class Shop_Beacon(models.Model):
+	shopname = models.CharField('shopname', default = 0)
+	shop = models.IntegerField('shop', default = 0)
+	beacon = models.IntegerField('beacon', default = 0)
+
+class Beacon(models.Model):
+	major = models.IntegerField('major', default = 0)
+	minor = models.IntegerField('minor', default = 0)
+
+class Hint(models.Model):
+	hint_num = models.IntegerField('hint_number', default = 0)
+	hint_sent = models.CharField('hint_sentence', max_length = 255, default = 'HINT')
+
+class Data(models.Model):
+	userdata = models.CharField('username', max_length = 255, default = 'NAME')
+	datavalue = models.CharField('shop', max_length = 255, default = 'SHOP')
