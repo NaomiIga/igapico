e86aadad7eefe22bdcbda314260988f897498e7f
伊賀尚美 <iganaomi@iga-MacBook-Pro.local>
2017-03-15 17:54:19 +0900

    Userの型変更

--- tekupico/cms/models.py
+++ tekupico/cms/models.py
@@ -10,8 +10,9 @@ class User(models.Model):
 	username = models.CharField('username', max_length = 255, default = 'NAME')
 	starttime = models.IntegerField('starttime', default = 0)
 	finishtime = models.IntegerField('finishtime', default = 0)
-	hint_action1_1 = models.IntegerField('hint1_1', default = 0)
 	treasure1 = models.IntegerField('treasure1', default = 0)
+	treasure2 = models.IntegerField('treasure2', default = 0)
+	treasure3 = models.IntegerField('treasure3', default = 0)
 	shopname = models.CharField('shopname', max_length = 255, default = 'SHOP')
 
 # User no hint siyou jyoukyou
@@ -31,13 +32,17 @@ class UsedHint(models.Model):
 # Not Change: treasure and beaconNo wo himoduke
 class Treasure_Beacon(models.Model):
 	treasure = models.IntegerField('treasure', default = 0)
-	beacon = models.IntegerField('beacon', default = 0)
+	beacon_id = models.IntegerField('beacon', default = 0)
+	major = models.IntegerField('major', default = 0)
+	minor = models.IntegerField('minor', default = 0)
 
 # Not Change: ikitaiSHOP to beaconNo wo himoduke
 class Shop_Beacon(models.Model):
 	shopname = models.CharField('shopname', max_length = 255, default = 'SHOPNAME')
 	shop_id = models.IntegerField('shop_id', default = 0)
-	beacon = models.IntegerField('beacon', default = 0)
+	beacon_id = models.IntegerField('beacon', default = 0)
+	major = models.IntegerField('major', default = 0)
+	minor = models.IntegerField('minor', default = 0)
 
 # Not Change: beacon list
 class Beacon(models.Model):
