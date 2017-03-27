ec817fd58d96b13739a6150f95be58a68717694d
tekupeko <rotsuki@mxdlab.net>
2017-03-27 18:53:18 +0900

    update models

--- tekupico/cms/models.py
+++ tekupico/cms/models.py
@@ -14,7 +14,7 @@ class User(models.Model):
 	#treasure1 = models.DateTimeField(default=timezone.now)
 	#treasure2 = models.DateTimeField(default=timezone.now)
 	#treasure3 = models.DateTimeField(default=timezone.now)
-	treasure1 = models.DateTimeField('1time', default=[0, 0, 0])
+	treasure1 = models.DateTimeField('1time', default=0)
 	treasure2 = models.DateTimeField('2time', default=0)
 	treasure3 = models.DateTimeField('3time', default=0)
 	shopname = models.CharField('shopname', max_length = 255, default = 'SHOP')
@@ -22,9 +22,9 @@ class User(models.Model):
 # User no hint siyou jyoukyou
 class UsedHint(models.Model):
 	username = models.CharField('username', max_length = 255, default = 'NAME')
-	treasure1 = models.DateTimeField(default=0)
-	treasure2 = models.DateTimeField(default=0)
-	treasure3 = models.DateTimeField(default=0)
+	treasure1 = models.DateTimeField('hint1-1', default=0)
+	treasure2 = models.DateTimeField('hint2-1', default=0)
+	treasure3 = models.DateTimeField('hint3-1', default=0)
 	#treasure1 = models.IntegerField('1_1time', default = 0)
 	#treasure1_2 = models.IntegerField('1_2time', default = 0)
 	#treasure1_3 = models.IntegerField('1_3time', default = 0)
