44f5502dfde1b64d54ca40b6c2b8f873f3bcef7d
伊賀尚美 <iganaomi@iga-MacBook-Pro.local>
2017-03-28 19:45:09 +0900

    ignore

--- tekupico/cms/admin.py
+++ tekupico/cms/admin.py
@@ -35,6 +35,43 @@ class KeyAreaAdmin(admin.ModelAdmin):
 	list_display = ('beacon_id', 'xgrid', 'ygrid',)
 	list_display_links = ('beacon_id',)
 
+class Shop_ladiesAdmin(admin.ModelAdmin):
+	list_display = ('shop_name',)
+
+class Shop_mensAdmin(admin.ModelAdmin):
+	list_display = ('shop_name',)
+
+class Shop_ladiesmensAdmin(admin.ModelAdmin):
+	list_display = ('shop_name',)
+
+class Shop_kidsAdmin(admin.ModelAdmin):
+	list_display = ('shop_name',)
+
+class Shop_sportsAdmin(admin.ModelAdmin):
+	list_display = ('shop_name',)
+
+class Shop_shoesbagAdmin(admin.ModelAdmin):
+	list_display = ('shop_name',)
+
+class Shop_fassiongoodsAdmin(admin.ModelAdmin):
+	list_display = ('shop_name',)
+
+class Shop_goodsvarietyAdmin(admin.ModelAdmin):
+	list_display = ('shop_name',)
+
+class Shop_accessoryAdmin(admin.ModelAdmin):
+	list_display = ('shop_name',)
+
+class Shop_foodAdmin(admin.ModelAdmin):
+	list_display = ('shop_name',)
+
+class Shop_serviceAdmin(admin.ModelAdmin):
+	list_display = ('shop_name',)
+
+class Shop_limitedAdmin(admin.ModelAdmin):
+	list_display = ('shop_name',)
+
+
 admin.site.register(User, UserAdmin)
 admin.site.register(UsedHint, UsedHintAdmin)
 admin.site.register(Beacon, BeaconAdmin)
@@ -43,3 +80,16 @@ admin.site.register(Treasure_Beacon, TreasureAdmin)
 admin.site.register(Hint, HintAdmin)
 admin.site.register(Data, DataAdmin)
 admin.site.register(KeyArea, KeyAreaAdmin)
+admin.site.register(Shop_ladies, Shop_ladiesmensAdmin)
+admin.site.register(Shop_mens, Shop_mensAdmin)
+admin.site.register(Shop_ladiesmens, Shop_ladiesmensAdmin)
+admin.site.register(Shop_kids, Shop_kidsAdmin)
+admin.site.register(Shop_sports, Shop_sportsAdmin)
+admin.site.register(Shop_shoesbag, Shop_shoesbagAdmin)
+admin.site.register(Shop_fassiongoods, Shop_fassiongoodsAdmin)
+admin.site.register(Shop_goodsvariety, Shop_goodsvarietyAdmin)
+admin.site.register(Shop_accessory, Shop_accessoryAdmin)
+admin.site.register(Shop_food, Shop_foodAdmin)
+admin.site.register(Shop_service, Shop_serviceAdmin)
+admin.site.register(Shop_limited, Shop_limitedAdmin)
+
--- tekupico/tekuPico/settings.py
+++ tekupico/tekuPico/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'cms',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -82,7 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': '/home/niga/igapico/tekupico/db.sqlite3',
-        'NAME': '/Users/royroy55/igapico/tekupico/db.sqlite3'
+        #'NAME': '/Users/royroy55/igapico/tekupico/db.sqlite3'
+        'NAME': '/Users/iganaomi/igapico/tekupico/db.sqlite3'
     }
 }
 
