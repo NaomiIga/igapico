71a4b595eee9b9aaf00938fe4d8593ab603d3a40
伊賀尚美 <iganaomi@iga-MacBook-Pro.local>
2017-03-31 15:18:54 +0900

    change views

--- tekupico/cms/views.py
+++ tekupico/cms/views.py
@@ -202,10 +202,10 @@ def shop_loading(request):
 		shops = []
 
 		if category == "ladies":
-			'''for i in Shop_ladies:
+			for i in len(Shop_ladies):
 				arr = Shop_ladies.objects.get()
-				shops.append = arr.shop_name'''
-			shops = Shop_ladies.objects.all()
+				shops.append = arr.shop_name
+			#shops = Shop_ladies.objects.all()
 
 		return JsonResponse({"data":shops})
 	else:
