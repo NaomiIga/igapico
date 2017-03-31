5e19911d395cd43d31c7e2ca0dd73c2593a14afc
伊賀尚美 <iganaomi@iga-MacBook-Pro.local>
2017-03-31 14:41:29 +0900

    change views

--- tekupico/cms/views.py
+++ tekupico/cms/views.py
@@ -200,7 +200,7 @@ def shop_loading(request):
 		category = datas["category"]   # ダブルクオート内はディクショナリーのキー
 
 		if category == "ladies":
-			shops = shop_ledies.objects.get()
+			shops = Shop_ladies.objects.get()
 
 		return JsonResponse({"data":shops})
 	else:
