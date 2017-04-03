09e505fd151ee2e2852c88a4b81c8be7730dd31e
伊賀尚美 <iganaomi@iga-MacBook-Pro.local>
2017-04-03 13:29:02 +0900

    change views デバッグ

--- tekupico/cms/views.py
+++ tekupico/cms/views.py
@@ -59,7 +59,7 @@ def pico_login(request):
 
 		
 		try:
-			testname = User.objects.get(username = name)
+			testname = User.objects.get(username = name[0])
 			return HTTPResponse(u'error')
 		except User.DoesNotExist:
 			new_data = User.objects.create(
