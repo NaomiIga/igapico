d8f26734341e7553a7c84b658afa8e90f1d4b38b
tekupeko <rotsuki@mxdlab.net>
2017-04-14 17:46:00 +0900

    update views

--- tekupico/cms/views.py
+++ tekupico/cms/views.py
@@ -536,8 +536,8 @@ def shop_loading(request):
 #csvとして出力する
 def export_csv(request):
 
-	img = Image.open("/home/niga/igapico/tekupico/cms/pictures/key.png")
-	response = HttpResponse(content_type='image/png')
+	#img = Image.open("/home/niga/igapico/tekupico/cms/pictures/key.png")
+	response = HttpResponse(open(‘/home/niga/igapico/tekupico/cms/pictures/key.png’,’rb’).read(), content_type='image/png')
 	response['Content-Disposition'] = 'attachment; filename="key.png"'
 
 	return response
