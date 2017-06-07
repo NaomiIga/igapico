610a14cdc8ad6a4e3b8f75d5b2256821f63628ad
royroy55 <rotsuki@mxdlab.net>
2017-06-07 18:19:28 +0900

    fix bug

--- tekupico/cms/views.py
+++ tekupico/cms/views.py
@@ -411,6 +411,7 @@ def treasure_check(request):
 		#update_data.treasure[treasure_number - 1] = getpointnow
 		treasure_list = update_data.treasures.split(',')
 		print treasure_list
+		print treasure_list[treasure_number - 1]
 		treasure_list[treasure_number - 1] = getpointnow
 		update_data.treasure = treasure_list
 
