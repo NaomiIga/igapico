a85d52d5e955e416ee03758eb2011a9394c50bd4
伊賀尚美 <iganaomi@iga-MacBook-Pro.local>
2018-04-30 14:46:03 +0900

    views

--- tekupico/cms/views.py
+++ tekupico/cms/views.py
@@ -111,7 +111,15 @@ def question(request):
 	userdata.key_time = KeyTime
 	userdata.save()
 
-	return JsonResponse({"key_time":KeyTime, "Threshold_RSSI":Threshold_RSSI})
+	TreasureInfo = []
+	for i in range(1, 11):
+		temp = []
+		treasuredata = Treasure_Beacon.objects.get(treasure = i)
+		temp.append(treasuredata.major)
+		temp.append(treasuredata.minor)
+		TreasureInfo.append(temp)
+
+	return JsonResponse({"key_time":KeyTime, "Threshold_RSSI":Threshold_RSSI, "TreasureInfo": TreasureInfo})
 
 #ユーザが行きたいショップをUserに保存
 @csrf_exempt
